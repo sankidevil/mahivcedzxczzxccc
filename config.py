1977f3e72064933a18a1df86ee6c56de5ac85c5a
RadhaRanp <166525375+RadhaRanp@users.noreply.github.com>
2024-05-19 11:56:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/INDIANCODERS-1/SHUKLA-MUSIC",
+    "https://github.com/RadhaRanp/edzxczzxccc",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
